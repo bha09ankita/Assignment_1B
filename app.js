d171c3654701abdc93b727b008bd75febe9f43e7
Pranav Shirke <pranav.shirke1311@gmail.com>
2022-03-28 12:48:33 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path");
 
-const app = express();
+const app = express(); 
 const port = process.env.PORT || 3000;
 
 // Setting path for public directory
